0bb332b99c8a3bae7e7e85a3f425de7eaf3f91c3
Artem Khobin <artem.khobin@gmail.com>
2016-10-10 20:04:38 +0300

    lesson2 added

---
+++ lesson2/src/lesson2/FirstProgram.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+class FirstProgram {
+
+  static int age = 24;
+
+  public static void main(String[] args) {
+    int i = 1_000_111;
+    printAge();
+  }
+
+  public static void printAge() {
+    System.out.println("Age: "+age);
+  }
+}
